049d9455765fb76202747557dc0b0e7f00983224
RaoAhmad <79688016+RaoAhmad@users.noreply.github.com>
2022-04-10 21:58:14 -0700

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./components/Header.js";
+import Navbar from "./components/Navbar/Navbar.js";
+
 
 
 function App() {
   return (
     <>
-      <Header Header="Rao Ahmad "
+
+      <Navbar Name=" R-Enterprises" />
+      <Header Header="Rao Ahmad Ali"
       />
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 const Header = (props) => <div>{props.Header}</div>
 export default Header;
